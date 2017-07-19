0be25e7afb90390ab914dc7cbddba1d862df1cd1
Arné Schreuder <arneschreuder@icloud.com>
2017-07-19 10:27:45 +0200

    feat(atom+latex): Atom packages + texmaker
    Signed-off-by: Arné Schreuder <arneschreuder@icloud.com>

--- atom/atom.symlink/packages/atom-typescript/node_modules/fsevents/build/Release/.deps/Users/arneschreuder/.dotfiles/atom/atom.symlink/packages/atom-typescript/lib/main/atom/tooltipManager.ts
+++ atom/atom.symlink/packages/atom-typescript/node_modules/fsevents/build/Release/.deps/Users/arneschreuder/.dotfiles/atom/atom.symlink/packages/atom-typescript/lib/main/atom/tooltipManager.ts
@@ -1,142 +1,143 @@
 // Inspiration : https://atom.io/packages/ide-haskell
 // and https://atom.io/packages/ide-flow
 
+import atomUtils = require("./utils") ///ts:import:generated
+import {clientResolver} from "../atomts"
 
-
-///ts:import=atomUtils
-import atomUtils = require('./atomUtils'); ///ts:import:generated
-///ts:import=parent
-import parent = require('../../worker/parent'); ///ts:import:generated
-
-import path = require('path');
-import fs = require('fs');
-import emissary = require('emissary');
-var Subscriber = emissary.Subscriber;
-import tooltipView = require('./views/tooltipView');
-import TooltipView = tooltipView.TooltipView;
-import {$} from "atom-space-pen-views";
-import escape = require('escape-html');
+import path = require("path")
+import fs = require("fs")
+import emissary = require("emissary")
+var Subscriber = emissary.Subscriber
+import tooltipView = require("./views/tooltipView")
+import TooltipView = tooltipView.TooltipView
+import {$} from "atom-space-pen-views"
+import escape = require("escape-html")
 
 export function getFromShadowDom(element: JQuery, selector: string): JQuery {
-    var el = element[0];
-    var found = (<any> el).rootElement.querySelectorAll(selector);
-    return $(found[0]);
+  var el = element[0]
+  var found = (<any>el).rootElement.querySelectorAll(selector)
+  return $(found[0])
 }
 
 export function attach(editorView: JQuery, editor: AtomCore.IEditor) {
-    var rawView: any = editorView[0];
+  var rawView: any = editorView[0]
 
-    // Only on ".ts" files
-    var filePath = editor.getPath();
-    var filename = path.basename(filePath);
-    var ext = path.extname(filename);
-    if (!atomUtils.isAllowedExtension(ext)) return;
+  // Only on ".ts" files
+  var filePath = editor.getPath()
+  if (!filePath) {
+    return
+  }
+  var filename = path.basename(filePath)
+  var ext = path.extname(filename)
+  if (!atomUtils.isAllowedExtension(ext)) return
 
-    // We only create a "program" once the file is persisted to disk
-    if (!fs.existsSync(filePath)) {
-        return;
+  // We only create a "program" once the file is persisted to disk
+  if (!fs.existsSync(filePath)) {
+    return
+  }
+
+  var clientPromise = clientResolver.get(filePath)
+  var scroll = getFromShadowDom(editorView, ".scroll-view")
+  var subscriber = new Subscriber()
+  var exprTypeTimeout: any | undefined
+  var exprTypeTooltip: TooltipView | undefined
+
+  // to debounce mousemove event's firing for some reason on some machines
+  var lastExprTypeBufferPt: any
+
+  subscriber.subscribe(scroll, "mousemove", (e: MouseEvent) => {
+    var pixelPt = pixelPositionFromMouseEvent(editorView, e)
+    var screenPt = editor.element.screenPositionForPixelPosition(pixelPt)
+    var bufferPt = editor.bufferPositionForScreenPosition(screenPt)
+    if (lastExprTypeBufferPt && lastExprTypeBufferPt.isEqual(bufferPt) && exprTypeTooltip) return
+
+    lastExprTypeBufferPt = bufferPt
+
+    clearExprTypeTimeout()
+    exprTypeTimeout = setTimeout(() => showExpressionType(e), 100)
+  })
+  subscriber.subscribe(scroll, "mouseout", () => clearExprTypeTimeout())
+  subscriber.subscribe(scroll, "keydown", () => clearExprTypeTimeout())
+
+  // Setup for clearing
+  editor.onDidDestroy(() => deactivate())
+
+  async function showExpressionType(e: MouseEvent) {
+    // If we are already showing we should wait for that to clear
+    if (exprTypeTooltip) return
+
+    var pixelPt = pixelPositionFromMouseEvent(editorView, e)
+    pixelPt.top += editor.element.getScrollTop()
+    pixelPt.left += editor.element.getScrollLeft()
+    var screenPt = editor.element.screenPositionForPixelPosition(pixelPt)
+    var bufferPt = editor.bufferPositionForScreenPosition(screenPt)
+    var curCharPixelPt = rawView.pixelPositionForBufferPosition([bufferPt.row, bufferPt.column])
+    var nextCharPixelPt = rawView.pixelPositionForBufferPosition([
+      bufferPt.row,
+      bufferPt.column + 1,
+    ])
+
+    if (curCharPixelPt.left >= nextCharPixelPt.left) return
+
+    // find out show position
+    var offset = editor.getLineHeightInPixels() * 0.7
+    var tooltipRect = {
+      left: e.clientX,
+      right: e.clientX,
+      top: e.clientY - offset,
+      bottom: e.clientY + offset,
+    }
+    exprTypeTooltip = new TooltipView(tooltipRect)
+
+    const client = await clientPromise
+    const result = await client
+      .executeQuickInfo({
+        file: filePath,
+        line: bufferPt.row + 1,
+        offset: bufferPt.column + 1,
+      })
+      .catch(err => undefined)
+
+    if (!result) {
+      return
     }
 
-    var scroll = getFromShadowDom(editorView, '.scroll-view');
-    var subscriber = new Subscriber();
-    var exprTypeTimeout = null;
-    var exprTypeTooltip: TooltipView = null;
+    const {displayString, documentation} = result.body!
 
-    // to debounce mousemove event's firing for some reason on some machines
-    var lastExprTypeBufferPt: any;
-
-    subscriber.subscribe(scroll, 'mousemove', (e) => {
-        var pixelPt = pixelPositionFromMouseEvent(editorView, e)
-        var screenPt = editor.screenPositionForPixelPosition(pixelPt)
-        var bufferPt = editor.bufferPositionForScreenPosition(screenPt)
-        if (lastExprTypeBufferPt && lastExprTypeBufferPt.isEqual(bufferPt) && exprTypeTooltip)
-            return;
-
-        lastExprTypeBufferPt = bufferPt;
-
-        clearExprTypeTimeout();
-        exprTypeTimeout = setTimeout(() => showExpressionType(e), 100);
-    });
-    subscriber.subscribe(scroll, 'mouseout', (e) => clearExprTypeTimeout());
-    subscriber.subscribe(scroll, 'keydown', (e) => clearExprTypeTimeout());
-
-    // Setup for clearing
-    editor.onDidDestroy(() => deactivate());
-
-    function showExpressionType(e: MouseEvent) {
-
-        // If we are already showing we should wait for that to clear
-        if (exprTypeTooltip) return;
-
-        var pixelPt = pixelPositionFromMouseEvent(editorView, e);
-        pixelPt.top += editor.getScrollTop();
-        pixelPt.left += editor.getScrollLeft();
-        var screenPt = editor.screenPositionForPixelPosition(pixelPt);
-        var bufferPt = editor.bufferPositionForScreenPosition(screenPt);
-        var curCharPixelPt = rawView.pixelPositionForBufferPosition([bufferPt.row, bufferPt.column]);
-        var nextCharPixelPt = rawView.pixelPositionForBufferPosition([bufferPt.row, bufferPt.column + 1]);
-
-        if (curCharPixelPt.left >= nextCharPixelPt.left) return;
-
-        // find out show position
-        var offset = (<any>editor).getLineHeightInPixels() * 0.7;
-        var tooltipRect = {
-            left: e.clientX,
-            right: e.clientX,
-            top: e.clientY - offset,
-            bottom: e.clientY + offset
-        };
-        exprTypeTooltip = new TooltipView(tooltipRect);
-
-        var position = atomUtils.getEditorPositionForBufferPosition(editor, bufferPt);
-
-        // Actually make the program manager query
-        parent.quickInfo({ filePath, position }).then((resp) => {
-            if (!resp.valid) {
-                hideExpressionType();
-            }
-            else {
-                var message = `<b>${escape(resp.name) }</b>`;
-                if (resp.comment) {
-                    message = message + `<br/><i>${escape(resp.comment).replace(/(?:\r\n|\r|\n)/g, '<br />') }</i>`;
-                }
-                // Sorry about this "if". It's in the code I copied so I guess its there for a reason
-                if (exprTypeTooltip) {
-                    exprTypeTooltip.updateText(message);
-                }
-            }
-        });
+    var message = `<b>${escape(displayString)}</b>`
+    if (documentation) {
+      message =
+        message + `<br/><i>${escape(documentation).replace(/(?:\r\n|\r|\n)/g, "<br />")}</i>`
     }
+    if (exprTypeTooltip) {
+      exprTypeTooltip.updateText(message)
+    }
+  }
 
-
-    function deactivate() {
-        subscriber.unsubscribe();
-        clearExprTypeTimeout();
+  function deactivate() {
+    subscriber.unsubscribe()
+    clearExprTypeTimeout()
+  }
+  /** clears the timeout && the tooltip */
+  function clearExprTypeTimeout() {
+    if (exprTypeTimeout) {
+      clearTimeout(exprTypeTimeout)
+      exprTypeTimeout = null
     }
-    /** clears the timeout && the tooltip */
-    function clearExprTypeTimeout() {
-        if (exprTypeTimeout) {
-            clearTimeout(exprTypeTimeout);
-            exprTypeTimeout = null;
-        }
-        hideExpressionType();
-    }
-    function hideExpressionType() {
-        if (!exprTypeTooltip) return;
-        exprTypeTooltip.$.remove();
-        exprTypeTooltip = null;
-    }
+    hideExpressionType()
+  }
+  function hideExpressionType() {
+    if (!exprTypeTooltip) return
+    exprTypeTooltip.$.remove()
+    exprTypeTooltip = undefined
+  }
 }
 
-
-function pixelPositionFromMouseEvent(editorView, event: MouseEvent) {
-    var clientX = event.clientX, clientY = event.clientY;
-    var linesClientRect = getFromShadowDom(editorView, '.lines')[0].getBoundingClientRect();
-    var top = clientY - linesClientRect.top;
-    var left = clientX - linesClientRect.left;
-    return { top: top, left: left };
-}
-
-function screenPositionFromMouseEvent(editorView, event) {
-    return editorView.getModel().screenPositionForPixelPosition(pixelPositionFromMouseEvent(editorView, event));
+function pixelPositionFromMouseEvent(editorView: JQuery, event: MouseEvent) {
+  var clientX = event.clientX,
+    clientY = event.clientY
+  var linesClientRect = getFromShadowDom(editorView, ".lines")[0].getBoundingClientRect()
+  var top = clientY - linesClientRect.top
+  var left = clientX - linesClientRect.left
+  return {top: top, left: left}
 }
--- atom/atom.symlink/packages/atom-typescript/node_modules/fsevents/build/Release/.deps/Users/arneschreuder/.dotfiles/atom/atom.symlink/packages/atom-typescript/lib/main/atom/views/renameView.ts
+++ atom/atom.symlink/packages/atom-typescript/node_modules/fsevents/build/Release/.deps/Users/arneschreuder/.dotfiles/atom/atom.symlink/packages/atom-typescript/lib/main/atom/views/renameView.ts
@@ -1,104 +1,124 @@
-import view = require('./view');
-var $ = view.$;
-var html = require('../../../../views/renameView.html');
+import view = require("./view")
+var $ = view.$
+var html = require("../../../../views/renameView.html")
 
-interface EditorView extends JQuery {
-    model: AtomCore.IEditor;
+interface EditorViewzz extends JQuery {
+  model: AtomCore.IEditor
 }
 
 interface RenameViewOptions {
-    autoSelect: boolean;
-    title: string;
-    text: string;
-    onCommit: (newValue: string) => any;
-    onCancel: () => any;
-    /** A truthy string return indicates a validation error */
-    onValidate: (newValue: string) => string;
-    openFiles: string[];
-    closedFiles: string[];
+  autoSelect: boolean
+  title: string
+  text: string
+  onCommit?: (newValue: string) => any
+  onCancel?: () => any
+  /** A truthy string return indicates a validation error */
+  onValidate: (newValue: string) => string
 }
 
-export class RenameView
-    extends view.View<RenameViewOptions> {
+export class RenameView extends view.View<RenameViewOptions> {
+  private newNameEditor: EditorViewzz
+  private validationMessage: JQuery
+  private panel: AtomCore.Panel
+  private title: JQuery
+  static content = html
 
-    private newNameEditor: EditorView;
-    private validationMessage: JQuery;
-    private fileCount: JQuery;
-    private title: JQuery;
-    static content = html;
-
-    public init() {
-        $(atom.views.getView(atom.workspace)).on('keydown', (e) => {
-            if (e.keyCode == 27) { // escape
-                if (this.options.onCancel) {
-                    this.options.onCancel();
-                    this.clearView();
-                }
-            }
-        });
-
-        this.newNameEditor.on('keydown', (e) => {
-            var newText = this.newNameEditor.model.getText();
-            if (e.keyCode == 13) { // enter
-                var invalid = this.options.onValidate(newText);
-                if (invalid) {
-                    this.validationMessage.text(invalid);
-                    this.validationMessage.show();
-                    return;
-                }
-                this.validationMessage.hide();
-
-                if (this.options.onCommit) {
-                    this.options.onCommit(newText);
-                    this.clearView();
-                }
-            }
-            if (e.keyCode == 27) { // escape
-                if (this.options.onCancel) {
-                    this.options.onCancel();
-                    this.clearView();
-                }
-            }
-        });
-    }
-
-    public editorAtRenameStart: AtomCore.IEditor = null;
-    public clearView() {
-        if (this.editorAtRenameStart && !this.editorAtRenameStart.isDestroyed()) {
-            var view = atom.views.getView(this.editorAtRenameStart);
-            view.focus();
+  public init() {
+    $(atom.views.getView(atom.workspace)).on("keydown", e => {
+      if (e.keyCode == 27) {
+        // escape
+        if (this.options.onCancel) {
+          this.options.onCancel()
+          this.clearView()
         }
-        panel.hide();
-        this.options = <any>{};
-        this.editorAtRenameStart = null;
-    }
+      }
+    })
 
-    public renameThis(options: RenameViewOptions) {
-        this.options = options;
-        this.editorAtRenameStart = atom.workspace.getActiveTextEditor();
-        panel.show();
-
-        this.newNameEditor.model.setText(options.text);
-        if (this.options.autoSelect) {
-            this.newNameEditor.model.selectAll();
+    this.newNameEditor.on("keydown", e => {
+      var newText = this.newNameEditor.model.getText()
+      if (e.keyCode == 13) {
+        // enter
+        var invalid = this.options.onValidate(newText)
+        if (invalid) {
+          this.validationMessage.text(invalid)
+          this.validationMessage.show()
+          return
         }
-        else {
-            this.newNameEditor.model.moveToEndOfScreenLine();
+        this.validationMessage.hide()
+
+        if (this.options.onCommit) {
+          this.options.onCommit(newText)
+          this.clearView()
         }
-        this.title.text(this.options.title);
-        this.newNameEditor.focus();
+      }
+      if (e.keyCode == 27) {
+        // escape
+        if (this.options.onCancel) {
+          this.options.onCancel()
+          this.clearView()
+        }
+      }
+    })
+  }
 
-        this.validationMessage.hide();
+  public setPanel(panel: AtomCore.Panel) {
+    this.panel = panel
+  }
 
-        this.fileCount.html(`<div>
-            Files Counts: <span class='highlight'> Already Open ( ${options.openFiles.length} )</span> and <span class='highlight'> Currently Closed ( ${options.closedFiles.length} ) </span>
-        </div>`);
+  public editorAtRenameStart?: AtomCore.IEditor
+  public clearView() {
+    if (this.editorAtRenameStart && !this.editorAtRenameStart.isDestroyed()) {
+      var view = atom.views.getView(this.editorAtRenameStart)
+      view.focus()
     }
+    this.panel.hide()
+    this.options = <any>{}
+    this.editorAtRenameStart = undefined
+  }
+
+  private renameThis(options: RenameViewOptions) {
+    this.options = options
+    this.editorAtRenameStart = atom.workspace.getActiveTextEditor()
+    this.panel.show()
+
+    this.newNameEditor.model.setText(options.text)
+    if (this.options.autoSelect) {
+      this.newNameEditor.model.selectAll()
+    } else {
+      this.newNameEditor.model.moveCursorToEndOfScreenLine()
+    }
+    this.title.text(this.options.title)
+    this.newNameEditor.focus()
+
+    this.validationMessage.hide()
+  }
+
+  // Show the dialog and resolve the promise with the entered string
+  showRenameDialog(options: RenameViewOptions): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.renameThis({
+        ...options,
+        onCancel: reject,
+        onCommit: resolve,
+      })
+    })
+  }
 }
 
-export var panelView: RenameView;
-var panel: AtomCore.Panel;
-export function attach() {
-    panelView = new RenameView(<any>{});
-    panel = atom.workspace.addModalPanel({ item: panelView, priority: 1000, visible: false });
+export function attach(): {dispose(): void; renameView: RenameView} {
+  const renameView = new RenameView(<any>{})
+  const panel = atom.workspace.addModalPanel({
+    item: renameView,
+    priority: 1000,
+    visible: false,
+  })
+
+  renameView.setPanel(panel)
+
+  return {
+    dispose() {
+      console.log("TODO: Detach the rename view: ", panel)
+    },
+    renameView,
+  }
 }
