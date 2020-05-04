b5de8aba5d37ff125c584eb01fc991d6d0c92bf2
Arné Schreuder <arneschreuder@Arnes-MacBook-Pro.local>
2020-05-04 23:22:02 +0200

    :construction: Save point

--- install.sh
+++ install.sh
@@ -10,27 +10,33 @@ if [[ "$(uname)" == "Darwin" ]]; then
   brew bundle --file brew/Brewfile
 fi
 
+if [[ ! -d ~/.oh-my-zsh ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	rm -rf ~/.oh-my-zsh
+fi
+
 if [[ $SHELL != *"zsh"* ]]; then
   echo "zsh"
   chsh -s /bin/zsh
 fi
 
-ln -sf $(pwd)/git/.gitignore ~/.gitignore
-ln -sf $(pwd)/git/.gitconfig ~/.gitconfig
-
 mkdir -p ~/.vim
 mkdir -p ~/.config/nvim
+
+ln -sf $(pwd)/git/.gitignore ~/.gitignore
+ln -sf $(pwd)/git/.gitconfig ~/.gitconfig
+ln -sf $(pwd)/base16/shell ~/.config/base16-shell
 ln -sf $(pwd)/nvim/init.vim ~/.vimrc
 ln -sf $(pwd)/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf $(pwd)/tmux/.tmux.conf ~/.tmux.conf
 ln -sf $(pwd)/zsh/.hushlogin ~/.hushlogin
+ln -sf $(pwd)/zsh/.oh-my-zsh/custom ~/.oh-my-zsh
 ln -sf $(pwd)/zsh/.zshrc ~/.zshrc
 
-if [[ ! -d ~/.oh-my-zsh ]]; then
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-fi
-
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl \
+	-fLo \
+	~/.local/share/nvim/site/autoload/plug.vim \
+	--create-dirs \
+    	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 nvim +PlugInstall +qall
--- nvim/init.vim
+++ nvim/init.vim
@@ -1,2 +1,7 @@
 call plug#begin('~/.local/share/nvim/plugged')
 call plug#end()
+
+if filereadable(expand("~/.vimrc_background"))
+  let base16colorspace=256
+  source ~/.vimrc_background
+endif
